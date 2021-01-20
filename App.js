27027721c0e9ef69fe7d3c1169eed61e530f61e4
benjaminclosson1 <benjaminclosson1@gmail.com>
2021-01-19 19:12:46 -0800

    played with styles

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
+  let x = 1;
   console.log("App Executed");
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Hello React-Native!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "dodgerblue",
   },
 });
